acaef4df5a01e259d3ae053297240ea626425991
Arman <trisaribaru@gmail.com>
2024-02-28 11:26:57 +0700

    shared widget

---
+++ lib/utility/shared/widgets/custom_date_picker.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import '../constants/constants_ui.dart';
+
+class CustomDatePicker extends StatefulWidget {
+  const CustomDatePicker(
+      {Key? key, required this.title, required this.dateinput})
+      : super(key: key);
+
+  final String title;
+  final TextEditingController dateinput;
+
+  @override
+  _CustomDatePickerState createState() => _CustomDatePickerState();
+}
+
+class _CustomDatePickerState extends State<CustomDatePicker> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          widget.title,
+          style: blackTextStyle.copyWith(
+            fontSize: 14,
+            fontWeight: semiBold,
+          ),
+        ),
+        const SizedBox(height: 10),
+        SizedBox(
+          // margin: EdgeInsets.only(right: 15),
+          width: MediaQuery.of(context).size.width / 2.5,
+          child: TextField(
+            controller: widget.dateinput, //editing controller of this TextField
+            decoration: InputDecoration(
+              hintText: "Tanggal",
+              hintStyle: greyTextStyle.copyWith(fontSize: 13),
+              suffixIcon: const Icon(Icons.arrow_forward),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(8),
+                borderSide: BorderSide(color: kGreyColor),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(8),
+                borderSide: BorderSide(color: kOrangeColor),
+              ),
+            ),
+            readOnly:
+                true, //set it true, so that user will not able to edit text
+            onTap: () async {
+              DateTime? pickedDate = await showDatePicker(
+                context: context,
+                initialDate: DateTime.now(),
+                firstDate: DateTime(
+                    2000), //DateTime.now() - not to allow to choose before today.
+                lastDate: DateTime(2101),
+              );
+
+              if (pickedDate != null) {
+                String formattedDate =
+                    DateFormat('yyyy-MM-dd').format(pickedDate);
+
+                setState(() {
+                  widget.dateinput.text =
+                      formattedDate; //set output date to TextField value.
+                });
+              } else {
+                print("Date is not selected");
+              }
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
